c439fe9613a9c3ee8406e29775f7628eb92843c4
piachimov <nadjabrco@gmail.com>
2024-05-20 11:46:40 +0300

    Update gitignore file

--- src/test/java/com/piachimov/springbootaws/SpringBootAwsApplicationTests.java
+++ src/test/java/com/piachimov/springbootaws/SpringBootAwsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.piachimov.springbootaws;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootAwsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.piachimov.springbootaws;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootAwsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
